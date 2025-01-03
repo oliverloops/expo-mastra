9b168fab63ade365536060a6cf6959a88387ca4e
evanbacon <bacon@expo.io>
2025-01-03 17:33:33 -0600

    Implement KeyboardFriendlyScrollView component and replace ScrollToBottomScrollView in MessagesScrollView

--- components/chatui.tsx
+++ components/chatui.tsx
@@ -1,175 +1,18 @@
 "use client";
 
 import { useActions, useUIState } from "ai/rsc";
-import React, { useCallback, useRef } from "react";
-import { View, ScrollViewProps, useWindowDimensions } from "react-native";
+import React from "react";
+import { Image, View, useWindowDimensions } from "react-native";
 
-import Animated, {
-  KeyboardState,
-  useAnimatedKeyboard,
-  useAnimatedRef,
-  useAnimatedStyle,
-  useDerivedValue,
-  useSharedValue,
-  scrollTo,
-} from "react-native-reanimated";
+import Animated from "react-native-reanimated";
 
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 import { AI } from "./ai-context";
 import { ChatToolbarInner } from "./chat-toolbar";
+import { KeyboardFriendlyScrollView } from "./keyboard-friendly-scrollview";
 
 const HEADER_HEIGHT = 0;
-
-function ScrollToBottomScrollView({ children, ...props }: ScrollViewProps) {
-  const ref = useAnimatedRef<Animated.ScrollView>();
-  const timeout = useRef<ReturnType<typeof setTimeout>>();
-  const keyboard = useAnimatedKeyboard();
-  const { bottom } = useSafeAreaInsets();
-  const scrollOffset = useSharedValue(0);
-  const lastKeyboardState = useSharedValue(KeyboardState.UNKNOWN);
-  const keyboardHeight = useSharedValue(0);
-  const scrollOffsetAtStart = useSharedValue(0);
-  const isTouching = useSharedValue(false);
-  const lastKeyboardPosition = useSharedValue(0);
-  const isScrollViewControlled = useSharedValue(false);
-
-  // A self-contained check to lazily determine how large the keyboard is when it's open.
-  useDerivedValue(() => {
-    if (keyboard.state.value === KeyboardState.OPEN) {
-      keyboardHeight.value = keyboard.height.value;
-    }
-  });
-
-  const scrollToBottom = useCallback(() => {
-    // console.log('scroll');
-
-    timeout.current && clearTimeout(timeout.current);
-    timeout.current = setTimeout(() => {
-      ref.current?.scrollToEnd({ animated: true });
-    }, 15);
-  }, [keyboard, ref.current]);
-
-  const scrollToBottomIfNotPanning = useCallback(() => {
-    if (
-      keyboard.state.value === KeyboardState.OPENING ||
-      keyboard.state.value === KeyboardState.CLOSING ||
-      isScrollViewControlled.value
-    ) {
-      // console.log(
-      //   'scrollToBottomIfNotPanning:bail',
-      //   keyboard.state.value,
-      //   isScrollViewControlled.value
-      // );
-      return;
-    }
-    scrollToBottom();
-  }, [keyboard, scrollToBottom]);
-
-  useDerivedValue(() => {
-    if (
-      !isScrollViewControlled.value &&
-      keyboard.state.value === KeyboardState.CLOSING &&
-      lastKeyboardState.value === KeyboardState.OPEN &&
-      isTouching.value
-    ) {
-      isScrollViewControlled.value = true;
-    }
-
-    if (
-      keyboard.state.value === KeyboardState.OPEN ||
-      keyboard.state.value === KeyboardState.CLOSED
-    ) {
-      if (
-        // Keyboard opened without user dragging.
-        // This means we can move to a controlled position without it being unexpected behavior.
-        !isScrollViewControlled.value &&
-        lastKeyboardState.value !== keyboard.state.value &&
-        keyboard.state.value === KeyboardState.OPEN
-      ) {
-        // console.log('open without input');
-        scrollTo(
-          ref,
-          0,
-          // Scroll to some maximum offset
-          Number.MAX_SAFE_INTEGER,
-          true
-        );
-      }
-
-      // Track that the native framework is no longer controlling the scroll view scrolling with the keyboard.
-      isScrollViewControlled.value = false;
-    }
-
-    // Track last keyboard state
-    if (lastKeyboardState.value !== keyboard.state.value) {
-      lastKeyboardState.value = keyboard.state.value;
-      scrollOffsetAtStart.value = scrollOffset.value;
-    }
-  });
-
-  useDerivedValue(() => {
-    if (isScrollViewControlled.value) {
-      // The native framework is controlling the scroll view scrolling with the keyboard, so we shouldn't animate it ourselves.
-      return;
-    }
-
-    if (keyboard.state.value === KeyboardState.OPENING) {
-      scrollTo(
-        ref,
-        0,
-        scrollOffsetAtStart.value + Math.max(0, keyboard.height.value - bottom),
-        false
-      );
-    } else if (keyboard.state.value === KeyboardState.CLOSING) {
-      scrollTo(
-        ref,
-        0,
-        scrollOffsetAtStart.value -
-          Math.max(0, keyboardHeight.value - keyboard.height.value - bottom), // Math.max(0, keyboard.height.value - bottom),
-        false
-      );
-    }
-
-    lastKeyboardPosition.value = keyboard.height.value;
-  });
-
-  const translateStyle2 = useAnimatedStyle(() => {
-    const h = Math.max(keyboard.height.value, bottom);
-    return {
-      minHeight: h,
-      maxHeight: h,
-    };
-  }, [bottom]);
-
-  return (
-    <Animated.ScrollView
-      {...props}
-      onTouchStart={() => {
-        isTouching.value = true;
-      }}
-      onTouchMove={() => {
-        isTouching.value = true;
-      }}
-      onTouchEnd={() => {
-        isTouching.value = false;
-      }}
-      onTouchCancel={() => {
-        isTouching.value = false;
-      }}
-      onScroll={(e) => {
-        scrollOffset.value = e.nativeEvent.contentOffset.y;
-      }}
-      onContentSizeChange={scrollToBottomIfNotPanning}
-      onLayout={scrollToBottomIfNotPanning}
-      scrollEventThrottle={16}
-      ref={ref}
-    >
-      {children}
-      <Animated.View style={translateStyle2} />
-    </Animated.ScrollView>
-  );
-}
 
 function MessagesScrollView() {
   const [messages] = useUIState<typeof AI>();
@@ -180,7 +23,7 @@ function MessagesScrollView() {
 
   return (
     <>
-      <ScrollToBottomScrollView
+      <KeyboardFriendlyScrollView
         style={{ flex: 1 }}
         contentInsetAdjustmentBehavior="automatic"
         keyboardDismissMode="interactive"
@@ -207,13 +50,13 @@ function MessagesScrollView() {
               flex: 1,
             }}
           >
-            {/* <Image
-              source={require("../assets/expo.png")}
-              style={{ width: 48, height: 48 }}
-            /> */}
+            <Image
+              source={require("@/assets/images/logo.dark.png")}
+              style={{ width: 128, height: 128 }}
+            />
           </View>
         )}
-      </ScrollToBottomScrollView>
+      </KeyboardFriendlyScrollView>
     </>
   );
 }
---
+++ components/keyboard-friendly-scrollview.tsx
@@ -0,0 +1,164 @@
+"use client";
+
+import React, { useCallback, useRef } from "react";
+import { ScrollViewProps } from "react-native";
+
+import Animated, {
+  KeyboardState,
+  useAnimatedKeyboard,
+  useAnimatedRef,
+  useAnimatedStyle,
+  useDerivedValue,
+  useSharedValue,
+  scrollTo,
+} from "react-native-reanimated";
+
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+export function KeyboardFriendlyScrollView({
+  children,
+  ...props
+}: ScrollViewProps) {
+  const ref = useAnimatedRef<Animated.ScrollView>();
+  const timeout = useRef<ReturnType<typeof setTimeout>>();
+  const keyboard = useAnimatedKeyboard();
+  const { bottom } = useSafeAreaInsets();
+  const scrollOffset = useSharedValue(0);
+  const lastKeyboardState = useSharedValue(KeyboardState.UNKNOWN);
+  const keyboardHeight = useSharedValue(0);
+  const scrollOffsetAtStart = useSharedValue(0);
+  const isTouching = useSharedValue(false);
+  const lastKeyboardPosition = useSharedValue(0);
+  const isScrollViewControlled = useSharedValue(false);
+
+  // A self-contained check to lazily determine how large the keyboard is when it's open.
+  useDerivedValue(() => {
+    if (keyboard.state.value === KeyboardState.OPEN) {
+      keyboardHeight.value = keyboard.height.value;
+    }
+  });
+
+  const scrollToBottom = useCallback(() => {
+    // console.log('scroll');
+
+    timeout.current && clearTimeout(timeout.current);
+    timeout.current = setTimeout(() => {
+      ref.current?.scrollToEnd({ animated: true });
+    }, 15);
+  }, [keyboard, ref.current]);
+
+  const scrollToBottomIfNotPanning = useCallback(() => {
+    if (
+      keyboard.state.value === KeyboardState.OPENING ||
+      keyboard.state.value === KeyboardState.CLOSING ||
+      isScrollViewControlled.value
+    ) {
+      return;
+    }
+    scrollToBottom();
+  }, [keyboard, scrollToBottom]);
+
+  useDerivedValue(() => {
+    if (
+      !isScrollViewControlled.value &&
+      keyboard.state.value === KeyboardState.CLOSING &&
+      lastKeyboardState.value === KeyboardState.OPEN &&
+      isTouching.value
+    ) {
+      isScrollViewControlled.value = true;
+    }
+
+    if (
+      keyboard.state.value === KeyboardState.OPEN ||
+      keyboard.state.value === KeyboardState.CLOSED
+    ) {
+      if (
+        // Keyboard opened without user dragging.
+        // This means we can move to a controlled position without it being unexpected behavior.
+        !isScrollViewControlled.value &&
+        lastKeyboardState.value !== keyboard.state.value &&
+        keyboard.state.value === KeyboardState.OPEN
+      ) {
+        // console.log('open without input');
+        scrollTo(
+          ref,
+          0,
+          // Scroll to some maximum offset
+          Number.MAX_SAFE_INTEGER,
+          true
+        );
+      }
+
+      // Track that the native framework is no longer controlling the scroll view scrolling with the keyboard.
+      isScrollViewControlled.value = false;
+    }
+
+    // Track last keyboard state
+    if (lastKeyboardState.value !== keyboard.state.value) {
+      lastKeyboardState.value = keyboard.state.value;
+      scrollOffsetAtStart.value = scrollOffset.value;
+    }
+  });
+
+  useDerivedValue(() => {
+    if (isScrollViewControlled.value) {
+      // The native framework is controlling the scroll view scrolling with the keyboard, so we shouldn't animate it ourselves.
+      return;
+    }
+
+    if (keyboard.state.value === KeyboardState.OPENING) {
+      scrollTo(
+        ref,
+        0,
+        scrollOffsetAtStart.value + Math.max(0, keyboard.height.value - bottom),
+        false
+      );
+    } else if (keyboard.state.value === KeyboardState.CLOSING) {
+      scrollTo(
+        ref,
+        0,
+        scrollOffsetAtStart.value -
+          Math.max(0, keyboardHeight.value - keyboard.height.value - bottom), // Math.max(0, keyboard.height.value - bottom),
+        false
+      );
+    }
+
+    lastKeyboardPosition.value = keyboard.height.value;
+  });
+
+  const translateStyle2 = useAnimatedStyle(() => {
+    const h = Math.max(keyboard.height.value, bottom);
+    return {
+      minHeight: h,
+      maxHeight: h,
+    };
+  }, [bottom]);
+
+  return (
+    <Animated.ScrollView
+      {...props}
+      onTouchStart={() => {
+        isTouching.value = true;
+      }}
+      onTouchMove={() => {
+        isTouching.value = true;
+      }}
+      onTouchEnd={() => {
+        isTouching.value = false;
+      }}
+      onTouchCancel={() => {
+        isTouching.value = false;
+      }}
+      onScroll={(e) => {
+        scrollOffset.value = e.nativeEvent.contentOffset.y;
+      }}
+      onContentSizeChange={scrollToBottomIfNotPanning}
+      onLayout={scrollToBottomIfNotPanning}
+      scrollEventThrottle={16}
+      ref={ref}
+    >
+      {children}
+      <Animated.View style={translateStyle2} />
+    </Animated.ScrollView>
+  );
+}
